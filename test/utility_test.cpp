70623ab3e1a4bf96e01f7c0443b5bb36318e8baa
ZEL <1362848545@qq.com>
2023-02-03 17:12:47 +0800

    zel

--- test/utility_test.cpp
+++ test/utility_test.cpp
@@ -44,10 +44,42 @@ TEST_CASE("testing Class IniFile") {
 
 TEST_CASE("testing Class Xml") {
 
+    // Xml s1;
+    // s1.SetName("student");
+    // s1.SetAttr("name", "curry");
+    // s1.SetAttr("id", 1);
+    // s1.SetAttr("hight", 1.75);
+    // s1.SetText("勇士总冠军");
+
+    // Xml s2;
+    // s2.SetName("student");
+    // s2.SetAttr("name", "curry");
+    // s2.SetText("勇士总冠军");
+
+    // Xml root;
+    // root.SetName("students");
+    // root[0] = s1;
+    // root.Append(s2);
+
+    // cout << root.AsString() << endl;
+
+    // // // 遍历子节点方式一
+    // // for (auto it = root.begin(); it != root.end(); it++) {
+    // //     cout << it->AsString() << endl;
+    // // }
+
+    // // 遍历子节点方式二
+    // for (int i = 0; i < root.size(); i++) {
+    //     cout << root[i].AsString() << endl;
+    // }
+
     Xml root1;
     root1.Load("../config/workflow.xml");
 
-    int id  = root1[0].attr("name");
+    int id = root1[0].attr("name");
     cout << root1[0].name() << id << endl;
+
+
+    root1.Save("../config/temp.xml");
 
 }
